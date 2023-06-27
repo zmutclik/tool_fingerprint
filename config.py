c8ee5ca2dcdb9f86a153f78364a6f87e82693025
Fahrudin Hariadi <fahrudin.hariadi@gmail.com>
2023-06-27 10:49:42 +0700

    bug

--- config.py
+++ config.py
@@ -1,13 +1,4 @@
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
-
-pathfile = os.path.normpath(os.path.dirname(
-    os.path.abspath(__file__)) + os.sep)
-pathfile = os.path.abspath(os.path.join(pathfile, '..'))
-dotenv_path = join(pathfile, '.env')
-
-appFP_path = os.environ.get("appFP_path")
-appFP_title =  os.environ.get("appFP_title")
-app_user =  os.environ.get("app_user")
-app_pass =  os.environ.get("app_pass")
+appFP_path = "C:\Program Files (x86)\Aplikasi Sidik Jari BPJS Kesehatan\After.exe"
+appFP_title =  "*Aplikasi Registrasi Sidik Jari*"
+app_user =  ""
+app_pass =  ""
